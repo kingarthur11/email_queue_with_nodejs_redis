65d458d9de2b11cbcf99d534388ac2dec7a22d41
arthur <orduharthur@gmail.com>
2021-02-20 00:14:08 +0100

    changes

--- app.js
+++ app.js
@@ -13,20 +13,36 @@ const sendMailQueue = new Queue('sendMail', {
         port: 6379,
     }
 })
-const data = {
-    email: 'orduharthur@gmail.com'
-}
+// const data = {
+//     email: 'arthurorduh@gmail.com'
+// }
+
 const options = {
     delay: 2000,
     attempts: 2
 }
-sendMailQueue.add(data, options);
 
-sendMailQueue.process(async job => {
-    return await sendMail(job.data.email)
+app.post('/send', function(req, res, next) {
+    const data = req.body
+    sendMailQueue.add(data, options);
+    // console.log(sendMailQueue)
 })
 
-// app.post('/send', function(req, res, next) {
+// var job = new cron('1 * * * * *', () => {
+    sendMailQueue.process(async job => {
+    return await sendMail(job.data.email)
+    // console.log(job.data.email)
+})
+
+// const sendData = () => {
+//     sendMailQueue.process(async job => {
+//     return await sendMail(job.data.email)
+//     // console.log(job.data.email)
+//     })
+// }
+
+
+
     function sendMail (email) {
         return new Promise((resolve, reject) => {
         var mailOptions = {
@@ -44,7 +60,7 @@ sendMailQueue.process(async job => {
                 pass: process.env.EMAIL_PASSWORD
             }
         };  
-    nodemailer.createTransport(mailConfig).sendMail(mailOptions, (err, info) => {
+    nodemailer.createTransport(mailConfig).sendMail(mailOptions, (error, info) => {
         if (error) {
             reject(error);
         } else {
@@ -53,7 +69,7 @@ sendMailQueue.process(async job => {
     })
         
     })}
-// })
+
 app.listen(5000, () => {
     console.log(`App listening on port ${PORT}`)
 })
