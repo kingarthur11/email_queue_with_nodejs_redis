422667f9271211be61e08ee4028c809ec48a8fe3
arthur <orduharthur@gmail.com>
2021-02-19 22:04:51 +0100

    changes

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const client = require('./helper/redisDatabase')
 const nodemailer = require('nodemailer')
 const Queue = require('bull')
 const cron = require('node-cron')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 5000;
+const app = express()
 
 const sendMailQueue = new Queue('sendMail', {
     redis: {
@@ -14,7 +14,7 @@ const sendMailQueue = new Queue('sendMail', {
     }
 })
 const data = {
-    email: 'arthurorduh@gmail.com'
+    email: 'orduharthur@gmail.com'
 }
 const options = {
     delay: 2000,
@@ -26,36 +26,34 @@ sendMailQueue.process(async job => {
     return await sendMail(job.data.email)
 })
 
-function sendMail (email) {
-    return new Promise((resolve, reject) => {
-    var mailOptions = {
-        from: 'arthurorduh2@gmail.com',
-        to: 'orduharthur@gmail.com',
-        subject: 'sending email using node js',
-        text: 'thank you for your time'
-    }   
-    var mailConfig = {
-        host: 'smtp.gmail.com',
-        port: 465,
-        service: 'gamil',
-        auth: {
-            user: 'arthurorduh2@gmail.com',
-            pass: process.env.EMAIL_PASSWORD
-        }
-    };
-    
-        nodemailer.createTransport(mailConfig).sendMail(mailOptions, (err, info) => {
+// app.post('/send', function(req, res, next) {
+    function sendMail (email) {
+        return new Promise((resolve, reject) => {
+        var mailOptions = {
+            from: 'arthurorduh2@gmail.com',
+            to: email,
+            subject: 'sending email using node js',
+            text: 'thank you for your time'
+        }   
+        var mailConfig = {
+            host: 'smtp.gmail.com',
+            port: 465,
+            service: 'gamil',
+            auth: {
+                user: 'arthurorduh2@gmail.com',
+                pass: process.env.EMAIL_PASSWORD
+            }
+        };  
+    nodemailer.createTransport(mailConfig).sendMail(mailOptions, (err, info) => {
         if (error) {
             reject(error);
         } else {
             resolve('Email sent: ' + info.response)
         }
-   
-})
-    
-})}
-const app = express()
-
+    })
+        
+    })}
+// })
 app.listen(5000, () => {
     console.log(`App listening on port ${PORT}`)
 })
